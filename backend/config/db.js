9f2e208157c953a4608a3f5a5d57f6365c4bd756
Priyanshu Singh <singhrajput2457@gmail.com>
2026-07-07 14:31:48 +0530

    deloy change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
-
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("connected to DB")
+const connectDB = async () => {
+    try {
+        if (!process.env.MONGO_URL) {
+            console.log('MONGO_URL is not set, skipping database connection');
+            return;
+        }
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('connected to DB');
+    } catch (error) {
+        console.log(error);
     }
-    catch(error){
-        console.log(error)
-    }
-}
-
+};
 
 module.exports = connectDB;
